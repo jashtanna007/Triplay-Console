4817c6d388bd22abb67f69f5a0030539ea96d61f
Krishiv100 <krishivpatel526@gmail.com>
2025-09-11 00:38:42 +0530

    added a function to access all positions

--- file1.cpp
+++ file1.cpp
@@ -2,36 +2,61 @@
 #include <vector>
 using namespace std;
 
-class box {
+class box
+{
     char nishaan;
+
 public:
-    box(){
-        nishaan=' '; 
-}
+    box() { nishaan = ' '; }
     char get() const { return nishaan; }
     void set(char m) { nishaan = m; }
+    bool empty() const { return nishaan == ' '; }
 };
 
-class board {
+class board
+{
     vector<vector<box>> grid;
+
 public:
-    board() {
-        grid=vector<vector<box>>(3, vector<box>(3));
-    }
-    void render_with_numbers() {
-        for (int i=0;i<3;i++) {
-            for (int j=0;j<3;j++) {
-                cout<<"x";
-                if (j<2) cout<<" | ";
+    board() { grid=vector<vector<box>>(3, vector<box>(3)); }
+    void buildblock()
+    {
+        int pos = 1;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (grid[i][j].empty())
+                {
+                    cout << pos;
+                }
+                else
+                {
+                    cout << grid[i][j].get();
+                }
+                if (j < 2)
+                    cout << " | ";
+                pos++;
             }
-            cout<<"\n";
-            if(i<2) cout<<"--+---+--\n";
+            cout << "\n";
+            if (i < 2)
+                cout << "--+---+--\n";
+        }
+    }
+    void place(int position, char symbol)
+    {
+        int row = (position - 1) / 3;
+        int col = (position - 1) % 3;
+        if (position >= 1 && position <= 9 && grid[row][col].empty())
+        {
+            grid[row][col].set(symbol);
         }
     }
 };
 
-int main() {
+int main()
+{
     board bd;
-    bd.render_with_numbers();
+    bd.buildblock();
     return 0;
 }
