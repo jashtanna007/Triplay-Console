ecf103b0819fafb45e3ca14df288dfaa8655c1dd
Sujal Bathani <sujalbathani@gmail.com>
2025-09-11 11:19:41 +0530

    modified user input

--- file1.cpp
+++ file1.cpp
@@ -1,62 +1,140 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class box
+class Box
 {
-    char nishaan;
+    char mark;
 
 public:
-    box() { nishaan = ' '; }
-    char get() const { return nishaan; }
-    void set(char m) { nishaan = m; }
-    bool empty() const { return nishaan == ' '; }
+    Box()
+    {
+        mark = ' ';
+    }
+    char getmark()
+    {
+        return mark;
+    }
+    void setmark(char m)
+    {
+        mark = m;
+    }
+    bool isempty()
+    {
+        if (mark == ' ')
+            return true;
+        return false;
+    }
 };
 
-class board
+class Board
 {
-    vector<vector<box>> grid;
+    Box grid[3][3];
 
 public:
-    board() { grid=vector<vector<box>>(3, vector<box>(3)); }
-    void buildblock()
+    void display()
     {
-        int pos = 1;
+        int index = 1;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                if (grid[i][j].empty())
+                if (grid[i][j].isempty())
                 {
-                    cout << pos;
+                    cout << index;
                 }
                 else
                 {
-                    cout << grid[i][j].get();
+                    cout << grid[i][j].getmark();
                 }
                 if (j < 2)
+                {
                     cout << " | ";
-                pos++;
+                }
+                index++;
             }
-            cout << "\n";
+            cout << endl;
             if (i < 2)
-                cout << "--+---+--\n";
+            {
+                cout << "--#---#--";
+            }
+            cout << endl;
         }
     }
-    void place(int position, char symbol)
+    bool place(int pos, char symbol)
     {
-        int row = (position - 1) / 3;
-        int col = (position - 1) % 3;
-        if (position >= 1 && position <= 9 && grid[row][col].empty())
+        int row = (pos - 1) / 3;
+        int col = (pos - 1) % 3;
+        if (pos >= 1 && pos <= 9 && grid[row][col].isempty())
         {
-            grid[row][col].set(symbol);
+            grid[row][col].setmark(symbol);
+            return true;
         }
+        return false;
     }
 };
 
 int main()
 {
-    board bd;
-    bd.buildblock();
+
+    int choice;
+    cout << "Enter your choice accordingly\n";
+    cout << "1.tic tac toe\n2.Maze runner\n3.knight tour\n\n";
+    cin >> choice;
+    cout << endl;
+    if (choice == 1)
+    {
+        Board bd;
+        // cout << "\nthe x is printing in the first row\n";
+        // bd.place(1, 'X');
+        int movesCount = 0;
+        char currSymbol = 'X';
+        string username1;
+        string username2;
+        cout << "ENter Player1 name \n";
+        cin >> username1;
+        cout << "ENter Player2 name \n";
+        cin >> username2;
+
+        string currentPlayer;
+
+        while (movesCount < 9)
+        {
+            bd.display();
+            if (currSymbol == 'X')
+            {
+                currentPlayer = username1;
+            }
+            else
+            {
+                currentPlayer = username2;
+            }
+            cout << endl
+                 << currentPlayer << "(" << currSymbol << ")" << "Enter position (1-9)" << endl;
+            int position;
+            cin >> position;
+            if (!bd.place(position, currSymbol))
+            {
+                cout << "Not a valid move !!! Enter a correct \n";
+            }
+            else
+            {
+                if(currSymbol == 'X' ){
+                    currSymbol = 'O';
+                }
+                else{
+                    currSymbol = 'X';
+                }
+                bd.place(position, currSymbol);
+                movesCount++;
+            }
+        }
+
+        bd.display();
+    }
+    else
+    {
+        cout << "work in progress\n";
+    }
+
     return 0;
 }
